ba3f96add058ce52ed8df2fe4d92357fbbc1f000
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-05 15:30:28 -0400

    Update album.js

--- photo-albums/album.js
+++ photo-albums/album.js
@@ -1,5 +1,5 @@
-
-
+let photoArr = document.querySelectorAll(".photo-container img");
+let currentIndex = 0;
 
 
 function showBig(_self) {
@@ -8,9 +8,30 @@ function showBig(_self) {
   
   BIG_DIV.style.display = "block";
   BIG_IMG.setAttribute("src", _self.src);
+  
+  for (let _i = 0; _i < photoArr.length; _i++) {
+    if (photoArr[_i].isEqualNode(_self)) currentIndex = _i;
+  }
+  
 }
 
 
 function hideBig(_self) {
   _self.style.display = "none"
+}
+
+window.addEventListener("keydown", (_key) => {
+  console.log(key.code);
+  if (_key.code == "ArrowRhight") increaseIndex();
+  else if (_key.code == "Arrowleft") decreaseIndex();
+})
+
+function increaseIndex() {
+  currentIndex++;
+  if (currentIndex >= photoArr.length) currentIndex = 0;
+  
+}
+
+function decreaseIndex() {
+  currentIndex--;
 }
