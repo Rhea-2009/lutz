2baef37e5a3984bbee4dca1e51e3a974ccbf9a54
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-07 15:28:09 -0400

    Create pythag.js

---
+++ wdsub/pythag.js
@@ -0,0 +1,24 @@
+
+
+
+function computePythag() {
+  let _inputArr = document.querrySelectorAll("input");
+  let _a = _inputArr[0].value;
+  let _b = _inputArr[1].value;
+  let _c = _inputArr[2].value;
+  
+  
+  if (_inputArr[0].value == "" && _inputArr[1].value != "" && _inputArr[2].value !="") {
+  _inputArr[0].value = Math.sqrt(Math.pow(_c, 2) - Math.pow(_b, 2));
+} else if (_inputArr[0].value != "" && _inputArr[1].value == "" && _inputArr[2].value !="") {
+  _inputArr[1].value = Math.sqrt(Math.pow(_c, 2) - Math.pow(_a, 2));
+} else if (_inputArr[0].value != "" && _inputArr[1].value != "" && _inputArr[2].value =="") {
+  _inputArr[2].value = Math.sqrt(Math.pow(_a, 2) + Math.pow(_b, 2));
+} else { //invalid input, display error
+  window.alert("Please enter two side lengths");
+}
+    
+  
+  
+  
+}
